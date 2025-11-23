a3bfd05c2d7c3af3b07d121d48eb097de7ad15fe
Aryo Abi Putra <aryoputra606@gmail.com>
2025-11-23 22:39:29 +0700

    first commit

---
+++ src/main/java/studyplanner/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package studyplanner;
+
+import studyplanner.ui.HomeFrame;
+
+/**
+ *
+ * @author aryop
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> new HomeFrame().setVisible(true));
+    }
+}
